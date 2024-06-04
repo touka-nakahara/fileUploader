7bf69f58cc32bc226d6c7ba7834e77318b1acc5f
touka-nakahara <167406123+touka-nakahara@users.noreply.github.com>
2024-06-04 11:57:51 +0900

    ignore log and delete cache

--- golang/cmd/main_test.go
+++ golang/cmd/main_test.go
@@ -27,9 +27,23 @@ func Test_slog(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			
+
 			slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)))
 			slog.Info("hello slog!", slog.String("user", "goher"))
+		})
+	}
+}
+
+func Test_osCreate(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{name: "serverStart"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			file, _ := os.Create("hoge")
+			defer file.Close()
 		})
 	}
 }
